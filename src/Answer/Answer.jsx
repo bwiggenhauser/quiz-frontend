e913f8008d5fe85b062e8e54feea4a31c6fd6463
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-28 23:19:32 +0200

    added answer component

---
+++ src/Answer/Answer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Answer(props) {
+	return (
+		<div className="flex justify-center items-center border-2 border-white w-2/3 h-16 mb-8 rounded-xl hover:bg-white hover:text-darkest-blue transition-all cursor-pointer">
+			<p>{props.text}</p>
+		</div>
+	);
+}
