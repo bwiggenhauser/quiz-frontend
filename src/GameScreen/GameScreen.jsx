74c21a2f6f13326522a6ba899b4125688e45a811
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-28 23:52:00 +0200

    added answer confirm button

--- src/GameScreen/GameScreen.jsx
+++ src/GameScreen/GameScreen.jsx
@@ -6,27 +6,34 @@ export default function GameScreen(props) {
 
 	return (
 		<div className="bg-darkest-blue flex flex-row justify-between items-start text-white">
-			
-            {/* Question Container */}
-            <div className="flex flex-col justify-center items-center w-2/3">
+			{/* Question Container */}
+			<div className="flex flex-col justify-center items-center w-2/3">
 				<h1 className="text-4xl py-8">
 					{data.current_question.question}
 				</h1>
 				{data.current_question.answers.map((answer) => (
 					<Answer text={answer} />
 				))}
+				<div className="flex justify-center items-center border-2 border-white w-2/5 h-16 mt-8 rounded-xl hover:bg-blue hover:border-blue transition-all cursor-pointer">
+					<p>Antwort abgeben</p>
+				</div>
 			</div>
 
-            {/* Player Scores */}
-            <div className="py-8 w-1/3 flex flex-col items-start justify-start">
-                <h1 className="text-4xl mb-8">Scoreboard</h1>
-                {data.player_scores.map((stats) => (
-                    <p className="text-xl mb-2">{stats.place}. {stats.player}: {stats.score}</p>
-                ))}
+			{/* Player Scores */}
+			<div className="py-8 w-1/3 flex flex-col items-start justify-start">
+				<h1 className="text-4xl mb-8">Scoreboard</h1>
+				{data.player_scores.map((stats) => (
+					<p className="text-xl mb-2">
+						{stats.place}. {stats.player}: {stats.score}
+					</p>
+				))}
 
-                <h1 className="text-4xl mt-16 mb-8">Status</h1>
-                <p className="text-xl mb-2">Runde: {data.round_info.current_round} / {data.round_info.total_rounds}</p>
-            </div>
+				<h1 className="text-4xl mt-16 mb-8">Status</h1>
+				<p className="text-xl mb-2">
+					Runde: {data.round_info.current_round} /{" "}
+					{data.round_info.total_rounds}
+				</p>
+			</div>
 		</div>
 	);
 }
