c265f8919f0d90ab73ace943f870038d1cb1bb72
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-29 01:59:39 +0200

    filled screen, added title styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import GameScreen from "./GameScreen/GameScreen";
 function App() {
 	console.log(data);
 	return (
-		<div className="App">
-			<h1>Quiz.io</h1>
+		<div className="App bg-darkest-blue h-screen">
+			<h1 className="text-white text-6xl font-extrabold text-center pt-8 pb-8">Quiz.io</h1>
 			<GameScreen data={data} />
 		</div>
 	);
