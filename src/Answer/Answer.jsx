5670a5c36d022779794d8e13b6dd850c4e645356
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-29 01:58:57 +0200

    added shadow to answer containers

--- src/Answer/Answer.jsx
+++ src/Answer/Answer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Answer(props) {
 	return (
-		<div className="flex justify-center items-center border-2 border-white w-4/5 h-16 mb-8 rounded-xl hover:bg-white hover:text-darkest-blue transition-all cursor-pointer">
+		<div className="flex justify-center items-center border-2 border-white w-4/5 h-16 mb-8 rounded-xl hover:bg-white hover:text-darkest-blue transition-all cursor-pointer shadow-xl">
 			<p>{props.text}</p>
 		</div>
 	);
