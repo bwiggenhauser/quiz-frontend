3ceeedcaaada03faf7c66ed93ee31d6c9d231f04
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-28 23:19:43 +0200

    added gamescreen to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import data from "./sampleData.json";
 import "./App.css";
+import GameScreen from "./GameScreen/GameScreen";
 
 function App() {
+	console.log(data);
 	return (
 		<div className="App">
 			<h1>Quiz.io</h1>
+			<GameScreen data={data} />
 		</div>
 	);
 }
