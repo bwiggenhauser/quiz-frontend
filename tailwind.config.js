59d7163f936c02ff8b6462181aa81478b3bbb883
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-28 23:18:56 +0200

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
+		colors: {
+			"darkest-blue": "#133c55ff",
+			"dark-blue": "#386fa4ff",
+			blue: "#59a5d8ff",
+			"baby-blue": "#84d2f6ff",
+			white: "#ffffff",
+		},
 	},
 	plugins: [],
 };
