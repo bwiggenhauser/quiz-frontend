96d4ef0625d4712c809eaaa7975fd0b423c4fc29
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-30 01:35:49 +0200

    renamed lobbyscreen -> landingscreen

--- src/LandingScreen/LandingScreen.jsx
+++ src/LandingScreen/LandingScreen.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-export default function LobbyScreen() {
+export default function LandingScreen(props) {
+	function joinLobby() {
+		const lobbyCode = document.getElementById("lobby-code-input").value;
+		document.getElementById("lobby-code-input").value = "";
+		props.joinFunction(lobbyCode);
+	}
+
 	return (
 		<div className="flex flex-col justify-start items-center mt-8">
-			
-            {/* Change Player Name */}
-            <div>
+			{/* Change Player Name */}
+			<div>
 				<input
 					type="text"
 					className="py-2 px-4 rounded-xl text-darkest-blue w-64 transition-all shadow-xl"
@@ -16,14 +21,17 @@ export default function LobbyScreen() {
 				</button>
 			</div>
 
-            {/* Join Lobby */}
-            <div className="mt-8">
+			{/* Join Lobby */}
+			<div className="mt-8">
 				<input
+					id="lobby-code-input"
 					type="text"
 					className="py-2 px-4 rounded-xl text-darkest-blue w-64 transition-all shadow-xl"
 					placeholder="Gib den Lobby-Code ein ..."
 				/>
-				<button className="transition-all bg-blue py-2 w-40 rounded-xl ml-4 text-white shadow-xl border-blue border-2 hover:border-white hover:bg-darkest-blue">
+				<button
+					onClick={joinLobby}
+					className="transition-all bg-blue py-2 w-40 rounded-xl ml-4 text-white shadow-xl border-blue border-2 hover:border-white hover:bg-darkest-blue">
 					Lobby beitreten
 				</button>
 			</div>
