c1054b6b6448d3c6e2e6aafb166f04ed29a7e934
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-29 13:07:13 +0200

    updated data on gamescreen from state

--- src/GameScreen/GameScreen.jsx
+++ src/GameScreen/GameScreen.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import Answer from "../Answer/Answer";
 
 export default function GameScreen(props) {
-	const data = props.data;
-
+	console.log(props);
 	return (
 		<div className="bg-darkest-blue flex flex-row justify-between items-start text-white">
 			{/* Question Container */}
 			<div className="flex flex-col justify-center items-center w-2/3">
 				<h1 className="text-4xl py-8">
-					{data.current_question.question}
+					{props.questions.question}
 				</h1>
-				{data.current_question.answers.map((answer) => (
+				{props.questions.answers.map((answer) => (
 					<Answer text={answer} />
 				))}
 				<div className="flex justify-center items-center border-2 border-white w-2/5 h-16 mt-8 rounded-xl hover:bg-blue hover:border-blue transition-all cursor-pointer shadow-xl">
@@ -22,7 +21,7 @@ export default function GameScreen(props) {
 			{/* Player Scores */}
 			<div className="py-8 w-1/3 flex flex-col items-start justify-start">
 				<h1 className="text-4xl mb-8">Scoreboard</h1>
-				{data.player_scores.map((stats) => (
+				{props.players.map((stats) => (
 					<p className="text-xl mb-2">
 						{stats.place}. {stats.player}: {stats.score}
 					</p>
@@ -30,8 +29,8 @@ export default function GameScreen(props) {
 
 				<h1 className="text-4xl mt-16 mb-8">Status</h1>
 				<p className="text-xl mb-2">
-					Runde: {data.round_info.current_round} /{" "}
-					{data.round_info.total_rounds}
+					Runde: {props.rounds.current_round} /{" "}
+					{props.rounds.total_rounds}
 				</p>
 
 				<h1 className="text-4xl mt-16 mb-8">Namen ändern</h1>
