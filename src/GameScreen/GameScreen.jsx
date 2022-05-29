dd41abafb3bcee9f44417b4a6a9b532a44c7a4ae
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-30 01:10:27 +0200

    decreased font sizes, removed name change

--- src/GameScreen/GameScreen.jsx
+++ src/GameScreen/GameScreen.jsx
@@ -6,41 +6,31 @@ export default function GameScreen(props) {
 		<div className="bg-darkest-blue flex flex-row justify-between items-start text-white">
 			{/* Question Container */}
 			<div className="flex flex-col justify-center items-center w-2/3">
-				<h1 className="text-4xl py-8">{props.questions.question}</h1>
+				<h1 className="text-2xl py-8">{props.questions.question}</h1>
 				{props.questions.answers.map((answer) => (
 					<Answer text={answer} />
 				))}
-				<div className="flex justify-center items-center border-2 border-white w-2/5 h-16 mt-8 rounded-xl hover:bg-blue hover:border-blue transition-all cursor-pointer shadow-xl">
+				<div className="flex justify-center items-center border-2 hover:border-white w-2/5 h-16 mt-8 rounded-xl bg-blue hover:bg-darkest-blue border-blue transition-all cursor-pointer shadow-xl">
 					<p>Antwort abgeben</p>
 				</div>
 			</div>
 
 			{/* Player Scores */}
 			<div className="py-8 w-1/3 flex flex-col items-start justify-start">
-				<h1 className="text-4xl mb-8">Scoreboard</h1>
+				<h1 className="text-2xl mb-8">Scoreboard</h1>
 				{props.players.map((player) => (
-					<p className="text-xl mb-2">
+					<p className="mb-2 text-base">
 						{player.place}. {player.player}: {player.score}
 					</p>
 				))}
 
 				<h1 className="text-4xl mt-16 mb-8">Status</h1>
-				<p className="text-xl mb-2">
+				<p className="text-base mb-2">
 					Runde: {props.rounds.current_round} /{" "}
 					{props.rounds.total_rounds}
 				</p>
 
-				<h1 className="text-4xl mt-16 mb-8">Namen ändern</h1>
-				<div>
-					<input
-						type="text"
-						className="py-2 px-4 rounded-xl text-darkest-blue w-64 transition-all shadow-xl"
-						placeholder="Ändere deinen Namen ..."
-					/>
-					<button className="transition-all bg-blue py-2 px-4 rounded-xl ml-4 text-white shadow-xl border-darkest-blue border-2 hover:border-white hover:bg-darkest-blue">
-						Namen ändern
-					</button>
-				</div>
+				
 			</div>
 		</div>
 	);
