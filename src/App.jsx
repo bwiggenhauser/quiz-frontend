62a43e62ba03fa91b7107f3c32d5a27d2c07239f
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-05-30 13:55:46 +0200

    added states, name change lobby join functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,42 @@ import data from "./sampleData.json";
 import "./App.css";
 import GameScreen from "./GameScreen/GameScreen";
 import LandingScreen from "./LandingScreen/LandingScreen";
+import Title from "./Title/Title";
+import LobbyScreen from "./LobbyScreen/LobbyScreen";
 
 function App() {
 	const [questionData, setQuestionData] = useState(data.current_question);
 	const [playerData, setPlayerData] = useState(data.player_scores);
 	const [roundData, setRoundData] = useState(data.round_info);
 	const [socket, setSocket] = useState(null);
+	const [status, setStatus] = useState("in-landing");
+	const [lobby, setLobby] = useState(null);
+	const [ownName, setOwnName] = useState("test");
 
 	const BACKEND_URL = "localhost:4005";
 
 	useEffect(() => {
 		const socket = io(BACKEND_URL);
 
-		socket.on("test", (data) => {
-			console.log(data);
+		socket.on("your-name", (data) => {
+			setOwnName(data);
 		});
 
-		socket.on("lobby-info", (data) => {
-			console.log(data);
+		socket.on("your-lobby", (data) => {
+			setStatus("in-lobby");
+			setLobby(data);
 		});
+
 		setSocket(socket);
 		return () => socket.close();
 	}, [setSocket]);
 
 	function joinLobby(lobbyCode) {
 		socket.emit("join-lobby", lobbyCode);
+	}
+
+	function changeMyName(newName) {
+		socket.emit("change-client-name", newName);
 	}
 
 	function addPlayer() {
@@ -40,23 +51,33 @@ function App() {
 		});
 		setPlayerData([...current]);
 	}
-
-	return (
-		<div className="App bg-darkest-blue h-screen">
-			<h1 className="text-white text-6xl font-extrabold text-center pt-8 pb-8">
-				Quiz.io
-			</h1>
-
-			<LandingScreen joinFunction={joinLobby} />
-
-			{/* <GameScreen
-				questions={questionData}
-				players={playerData}
-				rounds={roundData}
-			/>
-			<button onClick={addPlayer}>Add Player</button> */}
-		</div>
-	);
+	if (status === "in-landing") {
+		return (
+			<div className="App bg-darkest-blue h-screen">
+				<Title name={ownName} />
+				<LandingScreen
+					joinFunction={joinLobby}
+					changeNameFunction={changeMyName}
+				/>
+			</div>
+		);
+	} else if (status === "in-lobby") {
+		return (
+			<div className="App bg-darkest-blue h-screen">
+				<Title name={ownName} />
+				<LobbyScreen lobbyName={lobby} />
+			</div>
+		);
+	} else if (status == "in-game") {
+		return (
+			<div className="App bg-darkest-blue h-screen">
+				<Title name={ownName} />
+				<GameScreen />
+			</div>
+		);
+	} else {
+		alert("Status Error! Status = " + status);
+	}
 }
 
 export default App;
