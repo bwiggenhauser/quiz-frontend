b0e21b452b2f6de314fcef8d6757b789b3f0efb9
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-01 16:13:35 +0200

    added start game function and button

--- src/LobbyScreen/LobbyScreen.jsx
+++ src/LobbyScreen/LobbyScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 export default function LobbyScreen(props) {
-	console.log(props)
 	return (
 		<div className="flex flex-col justify-start items-center mt-8">
 			<h1 className="text-center text-white text-2xl">
@@ -9,10 +8,19 @@ export default function LobbyScreen(props) {
 			</h1>
 
 			{/* Lobby Members */}
-			<h1 className="mt-16 text-center text-white text-2xl">Spieler:</h1>
+			<h1 className="mt-16 pb-2 text-center text-white text-2xl">
+				Spieler:
+			</h1>
 			{props.members.map((e) => (
 				<p className="text-white text-xl text-center">{e}</p>
 			))}
+
+			{/* Start Game Button */}
+			<button
+				onClick={props.startGameFunctionme}
+				className="mt-16 transition-all bg-blue py-2 w-40 rounded-xl ml-4 text-white shadow-xl border-blue border-2 hover:border-white hover:bg-darkest-blue">
+				Spiel starten
+			</button>
 		</div>
 	)
 }
