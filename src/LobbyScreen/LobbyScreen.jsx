5a02020ea8de497d0cb6d0bd8f9f4c130ba8ce33
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-01 14:23:46 +0200

    added player list section in lobby

--- src/LobbyScreen/LobbyScreen.jsx
+++ src/LobbyScreen/LobbyScreen.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from "react"
 
 export default function LobbyScreen(props) {
-	console.log(props);
+	console.log(props)
 	return (
 		<div className="flex flex-col justify-start items-center mt-8">
 			<h1 className="text-center text-white text-2xl">
-				{"Lobby: " + props.lobbyName.lobby_name}
+				{"Lobby: " + props.lobbyName}
 			</h1>
+
+			{/* Lobby Members */}
+			<h1 className="mt-16 text-center text-white text-2xl">Spieler:</h1>
+			{props.members.map((e) => (
+				<p className="text-white text-xl text-center">{e}</p>
+			))}
 		</div>
-	);
+	)
 }
