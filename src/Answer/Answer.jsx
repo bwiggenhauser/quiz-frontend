9764cc86fda71108b670f683e92c5b5533cf3c9c
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-03 14:38:00 +0200

    added player answers to game screen answers

--- src/Answer/Answer.jsx
+++ src/Answer/Answer.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import "./Answer.css"
 
 export default function Answer(props) {
+	let players = ""
+	if (props.players !== undefined && props.players.length !== 0) {
+		players = ` (${props.players.toString().replaceAll(",", ", ")})`
+	}
+
 	return (
 		<div className="px-4 py-4 cursor-pointer bg-darkest-blue w-2/3 text-center mb-4 rounded-xl">
 			<input
@@ -13,8 +18,8 @@ export default function Answer(props) {
 			/>{" "}
 			<label
 				className="pl-4 text-xl cursor-pointer w-full"
-				for={props.text}>
-				{props.text}
+				htmlFor={props.text}>
+				{props.text + players}
 			</label>
 		</div>
 	)
