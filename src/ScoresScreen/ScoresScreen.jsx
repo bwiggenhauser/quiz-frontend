e0fdf11f9f425cbd1b8aef9178da88f0a8ee38e2
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-04 15:05:35 +0200

    added final scores screen

---
+++ src/ScoresScreen/ScoresScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function ScoresScreen(props) {
+	console.log(props)
+	return (
+		<div className="text-center text-white text-xl mt-8">
+			<h1 className="text-2xl">Scoreboard</h1>
+			{props.scores.map((e) => (
+				<p>
+					{e.name}: {e.score}
+				</p>
+			))}
+		</div>
+	)
+}
