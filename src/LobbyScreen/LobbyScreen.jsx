c262e9d0990ffd7ae59e225efd712989b0f3f9ff
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-13 21:26:37 +0200

    fixed wrong spacing

--- src/LobbyScreen/LobbyScreen.jsx
+++ src/LobbyScreen/LobbyScreen.jsx
@@ -22,7 +22,7 @@ export default function LobbyScreen(props) {
 
 					{/* Change Total Rounds */}
 					<div className="flex flex-row text-white items-center pt-2">
-						<p className="mr-4 text-base">Total rounds:</p>
+						<p className="text-base mr-2">Total rounds:</p>
 						<select
 							id="roundsSelect"
 							className="text-darkest-blue py-1 w-16 rounded-xl text-center"
@@ -42,14 +42,13 @@ export default function LobbyScreen(props) {
 			<div className="flex flex-col items-center bg-dark-blue w-1/5 py-8 rounded-xl shadow-xl mt-8">
 				{/* Lobby Members */}
 				<h1 className="text-2xl text-white font-bold">Players</h1>
-                <div className="pt-8">
-
-				{props.members.map((e) => (
-                    <p key={e} className="text-white text-base text-center">
-						{e}
-					</p>
-				))}
-                </div>
+				<div className="pt-8 flex items-start flex-col">
+					{props.members.map((e) => (
+						<p key={e} className="text-white text-base text-center py-1">
+							{e}
+						</p>
+					))}
+				</div>
 			</div>
 
 			{/* Start Game Button */}
