205797a2ef26e659dae92c98d208ab3de982ebf5
bwiggenhauser <58748871+bwiggenhauser@users.noreply.github.com>
2022-06-18 21:59:26 +0200

    changed start game button language

--- src/LobbyScreen/LobbyScreen.jsx
+++ src/LobbyScreen/LobbyScreen.jsx
@@ -55,7 +55,7 @@ export default function LobbyScreen(props) {
 			<button
 				onClick={startGame}
 				className="mt-16 transition-all bg-blue py-2 w-40 rounded-xl ml-4 text-white shadow-xl border-blue border-2 hover:border-white hover:bg-darkest-blue">
-				Spiel starten
+				Start Game
 			</button>
 		</div>
 	)
